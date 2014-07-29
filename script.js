9759ca8524642da9a67e74201f68c2c49a3c001c
aranginani <Anika@Anikas-MacBook-Pro.local>
2014-07-28 19:36:40 -0500

    changed names

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 jQuery('#calculate').click(function(){
 
 	var laundry=jQuery('[name="laundry"]').val()
+	var dineout=jQuery('[name="dineout"]').val()
+	var 
+
+
+
+	var dineout
 	console.log(laundry)
 	console.log("Hello")
 
